b0fd87efd4fb88fa02bd6c0366309c9bee2c050f
Azurehawke <goldenhawke@gmail.com>
2025-02-06 14:53:41 -0800

    w05 js done

--- lesson05/scripts/bom.js
+++ lesson05/scripts/bom.js
@@ -1,5 +1,55 @@
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
-const list = document.querySelector('_____');
+const list = document.querySelector('#list');
 
 button.addEventListener('click', function() {});
+
+function isDuplicate(chapter) {
+    const chapters = Array.from(list.querySelectorAll('li'));
+    return chapters.some(li => li.textContent.includes(chapter));
+}
+
+button.addEventListener('click', addChapter);
+input.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+        addChapter();
+    }
+});
+
+function addChapter() {
+    const chapter = input.value.trim();
+    
+    if (chapter === '') {
+        input.focus();
+        return;
+    }
+
+    if (isDuplicate(chapter)) {
+        alert('This Chapter is already listed!');
+        input.value = '';
+        input.focus();
+        return;
+    }
+
+    const li = document.createElement('li');
+    const deleteButton = document.createElement('button');
+
+    li.textContent = chapter;
+    deleteButton.textContent = '✖️';
+
+    li.append(deleteButton);
+    list.append(li);
+
+    deleteButton.addEventListener('click', function () {
+        list.removeChild(li);
+        input.focus();
+    });
+
+    input.value = '';
+    input.focus();
+}
+
+const el = document.getElementById('button');
+console.log(el.ariaLabel);
+el.ariaLabel = 'Add a Chapter';
+console.log(el.ariaLabel);
