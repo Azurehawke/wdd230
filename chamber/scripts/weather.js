53a53b7947e94b582210fb07d15cdc4e8428f5e9
Azurehawke <goldenhawke@gmail.com>
2025-03-13 15:17:29 -0700

    w10 chamber

---
+++ chamber/scripts/weather.js
@@ -0,0 +1,33 @@
+const currentTemp = document.querySelector('#temperature');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('#description');
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=34.54&lon=-117.29&appid=cee662efbfc91c6a11cf659402b99ce4&units=imperial`;
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            displayResults(data);
+        }
+        else {
+        const errorText = await response.text();
+        throw new Error(errorText);
+        }
+    } 
+    catch (error){
+        console.error('Error:', error);
+    }
+}
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${Math.round(data.main.temp)}°F`;
+    const iconSrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    weatherIcon.setAttribute('src', iconSrc);
+    let desc = data.weather[0].description;
+    captionDesc.textContent = `${desc}`;
+
+}
+
+apiFetch();
